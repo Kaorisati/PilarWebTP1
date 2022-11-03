c4c7e14746298f217ff68c8c002ffdc6d01030c9
kaorisati <kaorisati@gmail.com>
2022-11-03 16:23:54 -0300

    se arregla pagina dashboard

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -7,12 +7,18 @@ export const drawerMenu = [
     title: "pages",
     children: [
       {
-        title: "todo",
+        title: "Todo",
         path: "/todo",
+        image: require("../assets/images/todo.png"),
+        description:
+          "Descripción de Todo. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
       },
       {
-        title: "fetchList",
+        title: "FetchList",
         path: "/fetch-list",
+        image: require("../assets/images/pockeball.png"),
+        description:
+          "Descripción de fetchList. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
       },
     ],
   },
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,56 @@
-import React, { useEffect } from "react";
-import { Grid, Paper, Box } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Grid, Paper, Box, CardActionArea } from "@mui/material";
+import { drawerMenu } from "../../constants/menu";
+
+const Cards = (props) => {
+  const navigate = useNavigate();
+  const { children } = props.items[1];
+  console.log(children);
+  if (children) {
+    return children.map((child, index) => {
+      return (
+        <Card key={index + child.title} sx={{ maxWidth: 345, margin: 5 }}>
+          <CardActionArea onClick={() => navigate(child.path)}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={child.image}
+              alt="green iguana"
+              sx={{ objectFit: "contain" }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {child.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {child.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      );
+    });
+  }
+};
+
 const Dashboard = () => {
   return (
     <Grid container spacing={3}>
-      <Grid item md={6} xs={12}>
-        <Paper sx={{ p: 2 }}>
-          <Box>Dashboard</Box>
+      <Grid item md={12} xs={12}>
+        <Paper sx={{ p: 3 }}>
+          <Box>
+            <Typography gutterBottom variant="h5" component="div">
+              Dashboard
+            </Typography>
+            <Box display={"flex"}>
+              <Cards items={drawerMenu} />
+            </Box>
+          </Box>
         </Paper>
       </Grid>
     </Grid>
